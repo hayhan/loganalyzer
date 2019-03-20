db1bb5ebafeca02f31f3bfb9f222a4de06806d3b
Wei Han <wh935220@broadcom.com>
2019-03-20 17:59:39 +0800

    restruct table removing

--- logparser/logpurger.py
+++ logparser/logpurger.py
@@ -7,6 +7,16 @@ parentdir  = os.path.abspath(os.path.join(curfiledir, os.path.pardir))
 file       = open(parentdir + '/logs/test.txt', 'r')
 newfile    = open(parentdir + '/logs/test_new.txt', 'w')
 
+"""
+Definitions:
+primary line - no space proceeded
+nested line  - one or more spaces proceeded
+empty line   - LF or CRLF only in one line
+"""
+
+"""
+Patterns for removing timestamp, console prompt and others
+"""
 # The pattern for the timestamp added by console tool
 pattern0 = re.compile(r'\[(([01]\d|2[0-3]):([0-5]\d):([0-5]\d):(\d{3})|24:00:00:000)\]')
 # The pattern for CM console prompts
@@ -21,9 +31,55 @@ pattern5 = re.compile(r'\[[a-z ]*\] ', re.IGNORECASE)
 
 regexPatterns = [pattern0, pattern1, pattern2, pattern3, pattern4, pattern5]
 
+"""
+Patterns for removing Table headers
+"""
+title0  = re.compile(r' *Trimmed Candidate Downstream Service Group Settings List')
+title1  = re.compile(r' *sgid +size +member')
+title2  = re.compile(r' *Downstream Active Channel Settings')
+title3  = re.compile(r' *dcid +type +frequency')
+title4  = re.compile(r' *Upstream Active Channel Settings')
+title5  = re.compile(r' *ucid +rpt enable')
+title6  = re.compile(r' *BcmCmUsTargetMset \(a.k.a. usable UCDs gathered during startup\)')
+title7  = re.compile(r' *us +config')
+title8  = re.compile(r' *phy +change')
+title9  = re.compile(r' *type +ucid +dcid +count')
+title10 = re.compile(r' *REG-RSP-MP Summary:')
+title11 = re.compile(r' *TCC commands->')
+title12 = re.compile(r' *ucid +action +ranging strategy')
+title13 = re.compile(r' *Service Flow settings->')
+title14 = re.compile(r' *sfid +sid +ucids')
+title15 = re.compile(r' *DSID settings->')
+title16 = re.compile(r' *dsid +action +reseq')
+title17 = re.compile(r' *Active Downstream Channel Diagnostics')
+title18 = re.compile(r' *rx id +dcid +freq')
+title19 = re.compile(r' *plc +prfA')
+title20 = re.compile(r' *Active Upstream Channels:')
+title21 = re.compile(r' *rng +pwr')
+title22 = re.compile(r' *txid +ucid +dcid +sid')
+
+titlePatterns = [
+    title0,  title1,  title2,  title3,  title4,  title5,  title6,  title7,
+    title8,  title9,  title10, title11, title12, title13, title14, title15,
+    title16, title17, title18, title19, title20, title21, title22
+]
+
+"""
+Pattern for nested line
+"""
+nestedLinePattern = re.compile(r' +')
+
+"""
+Variables initialization
+"""
 inTable = False
 lastLineEmpty = False
 sccvEmptyLineCnt = 0
+
+"""
+1). Remove timestamps, console prompts, tables, empty lines
+2). Make an nested line as primary if two more empty lines proceeded
+"""
 for line in file:
     """
     Remove the unwanted strings which include some kind of timestamps, console prompts and etc.
@@ -36,8 +92,9 @@ for line in file:
     newline3 = pattern3.sub('', newline2)
     newline4 = pattern4.sub('', newline3)
     """
-
     newline = line
+
+    # Remove timestamp, console prompt and others
     for pattern in regexPatterns:
         newline = pattern.sub('', newline, count=1)
 
@@ -59,97 +116,14 @@ for line in file:
             # Still table line, remove it
             continue
 
-    # Remove specific table titles
-    match = re.match(r' *Trimmed Candidate Downstream Service Group Settings List', newline)
-    if match:
-        continue
-
-    match = re.match(r' *sgid  size  member', newline)
-    if match:
-        continue
-
-    match = re.match(r' *Downstream Active Channel Settings', newline)
-    if match:
-        continue
-
-    match = re.match(r' *dcid      type  frequency', newline)
-    if match:
-        continue
-
-    match = re.match(r' *Upstream Active Channel Settings', newline)
-    if match:
-        continue
-
-    match = re.match(r' *ucid  rpt enable', newline)
-    if match:
-        continue
-
-    match = re.match(r'BcmCmUsTargetMset (a.k.a. usable UCDs gathered during startup):', newline)
-    if match:
-        continue
-
-    match = re.match(r'   us               config', newline)
-    if match:
-        continue
-
-    match = re.match(r'   phy              change', newline)
-    if match:
-        continue
-
-    match = re.match(r'  type  ucid  dcid  count', newline)
-    if match:
-        continue
-
-    match = re.match(r'REG-RSP-MP Summary:', newline)
-    if match:
-        continue
-
-    match = re.match(r'TCC commands->', newline)
-    if match:
-        continue
-
-    match = re.match(r'ucid      action             ranging strategy', newline)
-    if match:
-        continue
-
-    match = re.match(r'Service Flow settings->', newline)
-    if match:
-        continue
-
-    match = re.match(r'sfid         sid              ucids', newline)
-    if match:
-        continue
-
-    match = re.match(r'DSID settings->', newline)
-    if match:
-        continue
-
-    match = re.match(r'dsid         action    reseq', newline)
-    if match:
-        continue
-
-    match = re.match(r'Active Downstream Channel Diagnostics:', newline)
-    if match:
-        continue
-
-    match = re.match(r'  rx id  dcid    freq', newline)
-    if match:
-        continue
-
-    match = re.match(r'                           plc  prfA', newline)
-    if match:
-        continue
-
-    match = re.match(r'Active Upstream Channels:', newline)
-    if match:
-        continue
-
-    match = re.match(r'                    rng     pwr', newline)
-    if match:
-        continue
-
-    match = re.match(r' txid  ucid  dcid   sid', newline)
-    if match:
+    # Remove specific table title line
+    goNextLine = False
+    for pattern in titlePatterns:
+        match = pattern.match(newline)
+        if match:
+            goNextLine = True
+            break
+    if goNextLine == True:
         continue
 
     # It is time to remove empty line
@@ -167,7 +141,10 @@ for line in file:
         # Update lastLineEmpty for the next line processing
         lastLineEmpty = False
 
-    # Process nested line now
+    # Make an nested line as primary if two more empty lines proceeded
+    if nestedLinePattern.match(newline):
+        if (lastLineEmpty == True) and (sccvEmptyLineCnt >= 2):
+            newline = newline.strip()
 
     newfile.write(newline)
 
