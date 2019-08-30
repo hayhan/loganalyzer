23ffe2c57da0eead5131a6eac48c0311c299d397
Wei Han <wh935220@broadcom.com>
2019-08-30 14:32:19 +0800

    checkin lost files

---
+++ entrance/runme.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+python3 ../logparser/logpurger.py
+python3 ../logparser/labelprocess.py
+python3 ../logparser/Drain_DOCSIS_demo.py
+
+python3 ../detector/demo/DecisionTree_demo.py
+#python3 ../detector/demo/LR_demo.py
+#python3 ../detector/demo/SVM_demo.py
