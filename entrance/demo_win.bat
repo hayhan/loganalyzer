0ee070b00278910d55dad8df383131d0c71b5f4f
Wei Han <wh935220@broadcom.com>
2019-09-18 18:04:19 +0800

    checkin demo for win

---
+++ entrance/demo_win.bat
@@ -0,0 +1,7 @@
+python ../logparser/logpurger.py
+python ../logparser/labelprocess.py
+python ../logparser/Drain_DOCSIS_demo.py
+
+python ../detector/demo/SupervisedLearning_pred.py
+
+python ../oldschool/analyzer.py
