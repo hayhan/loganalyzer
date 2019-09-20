70c5619f3a07674e20a50a6e72ec7420865cc263
Wei Han <wh935220@broadcom.com>
2019-09-20 16:34:36 +0800

    chmod 755 on train_unix.sh in entrance

--- entrance/train_unix.sh
+++ entrance/train_unix.sh
@@ -3,19 +3,19 @@
 # Process train dataset
 echo TRAINING=1 > config.txt
 # Preprocess and label raw log
-python ../logparser/logpurger.py
-python ../logparser/labelprocess.py
+python3 ../logparser/logpurger.py
+python3 ../logparser/labelprocess.py
 # Parse the log and generate templates ...
-python ../logparser/Drain_DOCSIS_demo.py
+python3 ../logparser/Drain_DOCSIS_demo.py
 
 # Process test dataset
 echo TRAINING=0 > config.txt
-python ../logparser/logpurger.py
-python ../logparser/labelprocess.py
+python3 ../logparser/logpurger.py
+python3 ../logparser/labelprocess.py
 # Parse the log and generate templates ...
-python ../logparser/Drain_DOCSIS_demo.py
+python3 ../logparser/Drain_DOCSIS_demo.py
 
 # Train and test on different models
-python ../detector/demo/DecisionTree_demo.py
-python ../detector/demo/LR_demo.py
-python ../detector/demo/SVM_demo.py
+python3 ../detector/demo/DecisionTree_demo.py
+python3 ../detector/demo/LR_demo.py
+python3 ../detector/demo/SVM_demo.py
