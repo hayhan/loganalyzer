008af7dbce4c3e91bcbf842517dfbf97825bd4d2
Wei Han <wh935220@broadcom.com>
2020-03-20 16:13:37 +0800

    win script to concatenate multiple train files into one

--- entrance/top_static_win.bat
+++ entrance/top_static_win.bat
@@ -1,6 +1,15 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-copy ..\logs\raw\log_0_3390_labeled.txt ..\logs\train.txt > nul
+
+rem ---Concatenate multiple raw files into one
+rem ---parameters: script inputLoc filenames outputLoc
+rem ---format: !="!^ is used for spliting a long string
+set fileList="log_0_3390_labeled.txt/log_2_3390_labeled.txt/!="!^
+log_3_3390_labeled.txt/log_4_3390_labeled.txt"
+
+python ..\tools\cat_files.py logs/raw %fileList% logs/train.txt
+
 copy ..\logs\raw\log_2_3390_labeled.txt ..\logs\test.txt > nul
 call .\train_static_win.bat
 copy ..\logs\raw\log_1_3390_labeled.txt ..\logs\test.txt > nul
