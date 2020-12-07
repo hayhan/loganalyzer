5675252e6f77bbbff17403af629d88f750e3fde8
wei Han <wh935220@broadcom.com>
2020-12-07 16:54:31 +0800

    fix a bug that cannot write the config.txt

--- entrance/old_school.sh
+++ entrance/old_school.sh
@@ -14,7 +14,7 @@ echo MODEL=DT
 echo WINDOW_SIZE=10000
 echo WINDOW_STEP=5000
 echo TEMPLATE_LIB_SIZE=2000
-) > config.txt
+) > $SHELL_FOLDER/config.txt
 
 # Adapt boardfarm CM logs
 #python3 $SHELL_FOLDER/../adapter/boardfarm_cm.py
