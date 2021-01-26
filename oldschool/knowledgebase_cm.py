494ce1cea8bc3dfe6d46d20ddd42af71d42f785e
Wei Han <wh935220@broadcom.com>
2021-01-26 14:45:28 +0800

    Update knowledgebase for oss

--- oldschool/knowledgebase_cm.py
+++ oldschool/knowledgebase_cm.py
@@ -52,25 +52,22 @@ def domain_knowledge(template_id, param_list):
     # But the break can be used in for loop.
     #
     for case in switch(template_id):
-        if case('c481c3c2'):
-            # TEMPLATE: "Telling application we lost lock on QAM channel <*>"
-            log_fault = True
-            log_description = "Lost lock happens on QAM channel {0}".format(param_list[0])
-            log_suggestion = "disconnected rf cable or low power"
-            break
 
+        # ---------------------------------------------------------------------
+        # Downstream channel status, lock failures
+        # ---------------------------------------------------------------------
         if case('bd6df2e3'):
             # TEMPLATE: "DS channel status rxid <*> dcid <*> freq <*> qam <*> fec <*> snr <*> power <*> mod <*>"
             # Check qam and fec status
             if param_list[3] == 'n' or param_list[4] == 'n':
                 log_fault = True
                 log_description = "QAM/FEC is not locked on DS channle {0}, freq {1}".format(param_list[0], param_list[2])
-                log_suggestion = "low power, big noise ...\n"
+                log_suggestion = "Low power, big noise ...\n"
             # Check DS SNR comparing to threshold. QAM64 18+3=21dB, QAM256 24+3=27dB. Some margins were added.
             if (param_list[7] == 'Qam64' and int(param_list[5]) <= 21) or (param_list[7] == 'Qam256' and int(param_list[5]) <= 27):
                 log_fault = True
                 log_description += "\nSNR is low below the threshold and it's not stable"
-                log_suggestion += "\nlow power, noise or bad board design all contribute to low SNR"
+                log_suggestion += "\nLow power, noise or bad board design all contribute to low SNR"
             # Check the rx power. -15dBmV ~ +15dBmV per spec. Warning if out of this range.
             if int(param_list[6]) > 15 or int(param_list[6]) < -15:
                 log_fault = True
@@ -78,6 +75,23 @@ def domain_knowledge(template_id, param_list):
                 log_suggestion += "\nAdjust the attanuator on DS link. The DS power at 0dBmV is better."
             break
 
+        if case('c481c3c2'):
+            # TEMPLATE: "Telling application we lost lock on QAM channel <*>"
+            log_fault = True
+            log_description = "Lost lock happens on QAM channel {0}".format(param_list[0])
+            log_suggestion = "Disconnected/Bad RF cable, low DS power, etc. Replace cable, decrease DS attanuation ..."
+            break
+
+        if case('9f88e081'):
+            # TEMPLATE: "BcmCmDsChan:: DsLockFail: hwRxId= <*> dcid= <*> -> enter kDsOperLockToRescueCmts state"
+            log_fault = True
+            log_description = "DS unlock happens on h/w channel {0}, dcid {1}".format(param_list[1], param_list[2])
+            log_suggestion = "Downstream is broken ..."
+            break
+
+        # ---------------------------------------------------------------------
+        # Upstream channel status
+        # ---------------------------------------------------------------------
         if case('6e45cf29'):
             # TEMPLATE: "US channel status txid <*> ucid <*> dcid <*> rngsid <*> power <*> freqstart <*> freqend <*> symrate <*> phytype <*> txdata <*>"
             # Check the tx power. Warning if out of 7dBmv ~ 51dBmV. Based on bonding channels and QAMs, 51~61dBmV can be reached but ignore it here.
@@ -91,36 +105,7 @@ def domain_knowledge(template_id, param_list):
                 log_fault = True
                 log_description += "\nUS path has no data, Ranging is NOT ok on channel {0} ucid {1} freq {2}" \
                                   .format(param_list[0], param_list[1], param_list[5])
-                log_suggestion += "\nRanging issue, check the ranging process"
-            break
-
-        if case('33de59d1'):
-            # TEMPLATE: "RNG-RSP UsChanId= <*> Stat= <*> "
-            if param_list[1] == 'Abort':
-                log_fault = True
-                log_description = "The ranging process is terminated by CMTS on ucid {0}".format(param_list[0])
-                log_suggestion = "Attanuation of upstream is too low or high usually."
-            break
-
-        if case('247a95a1'):
-            # TEMPLATE: "Logging event: Unicast Ranging Received Abort Response - Re-initializing MAC; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
-            log_fault = True
-            log_description = "Ranging is Aborted by CMTS and MAC will be reset"
-            log_suggestion = "Attanuation of upstream is too low or high usually."
-            break
-
-        if case('85511099'):
-            # TEMPLATE: "BcmCmUsRangingState:: T2NoInitMaintEvent: ERROR - no Init Maint map op -> restart error"
-            log_fault = True
-            log_description = "CM cannot receive broadcast init ranging opportunities"
-            log_suggestion = "Usually downstream is broken. Check downstream ..."
-            break
-
-        if case('2fc6ea2f'):
-            # TEMPLATE: "BcmCmUsRangingState:: T4NoStationMaintEvent: ERROR - no Station Maint map op error. hwTxId= <*> docs ucid= <*>"
-            log_fault = True
-            log_description = "T4 timeout happens on Tx channel {0} ucid {1}".format(param_list[0], param_list[1])
-            log_suggestion = "Usually downstream or upstream has big issues and mac reset might happen"
+                log_suggestion += "\nRanging issue, check the ranging process with other warnings / errors."
             break
 
         if case('9701dcb3'):
@@ -133,13 +118,71 @@ def domain_knowledge(template_id, param_list):
                             "need check the diplexer h/w to see if it supports"
             break
 
-        if case('9f88e081'):
-            # TEMPLATE: "BcmCmDsChan:: DsLockFail: hwRxId= <*> dcid= <*> -> enter kDsOperLockToRescueCmts state"
+        # ---------------------------------------------------------------------
+        # Ranging, T2/T3/T4 ... timeout
+        # ---------------------------------------------------------------------
+        if case('85511099'):
+            # TEMPLATE: "BcmCmUsRangingState:: T2NoInitMaintEvent: ERROR - no Init Maint map op -> restart error"
             log_fault = True
-            log_description = "DS unlock happens on h/w channel {0}, dcid {1}".format(param_list[1], param_list[2])
-            log_suggestion = "Downstream is broken ..."
+            log_description = "CM cannot receive broadcast init ranging opportunities"
+            log_suggestion = "Usually downstream is broken. Check downstream ..."
             break
 
+        if case('2fc6ea2f'):
+            # TEMPLATE: "BcmCmUsRangingState:: T4NoStationMaintEvent: ERROR - no Station Maint map op error. hwTxId= <*> docs ucid= <*>"
+            log_fault = True
+            log_description = "T4 timeout happens on Tx channel {0} ucid {1}".format(param_list[0], param_list[1])
+            log_suggestion = "Usually downstream or upstream has big issues and mac reset might happen."
+            break
+
+        if case('33de59d1'):
+            # TEMPLATE: "RNG-RSP UsChanId= <*> Stat= <*> "
+            if param_list[1] == 'Abort':
+                log_fault = True
+                log_description = "The ranging process is terminated by CMTS on ucid {0}".format(param_list[0])
+                log_suggestion = "Attanuation of upstream is too low or high usually. Adjust the US attnuation."
+            break
+
+        if case('247a95a1'):
+            # TEMPLATE: "Logging event: Unicast Ranging Received Abort Response - Re-initializing MAC; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            log_fault = True
+            log_description = "Ranging is Aborted by CMTS and MAC will be reset"
+            log_suggestion = "Attanuation of upstream is too low or high usually. Adjust the US attnuation."
+            break
+
+        if case('67d9799e'):
+            # TEMPLATE: "Logging event: No Ranging Response received - T3 time-out; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            log_fault = True
+            log_description = "T3 time out happens. CM cannot receive RNG-RSP from CMTS."
+            log_suggestion = "Usually the US attnuation is too high or low, or some kind of CMTS issues ..."
+            break
+
+        if case('738aa70f'):
+            # TEMPLATE: "Logging event: Started Unicast Maintenance Ranging - No Response received - T3 time-out; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            log_fault = True
+            log_description = "T3 time out happens for Unicast Maintenance Ranging."
+            log_suggestion = "Usually the US attnuation is too high or low, or some kind of CMTS issues ..."
+            break
+
+        if case('bd66bf4c') or case('ea78b302') or case('b5dae8ef'):
+            # TEMPLATE: "BcmCmUsRangingState:: T3NoRngRspEvent: ERROR - no more RNG-REQ retries."
+            # TEMPLATE: "Logging event: <*> consecutive T3 timeouts while trying to range on upstream channel <*>; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            # TEMPLATE: "Logging event: Ranging Request Retries exhausted; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            log_fault = True
+            log_description = "T3 time out happens. CM cannot receive RNG-RSP from CMTS."
+            log_suggestion = "Usually the US attnuation is too high or low, or some kind of CMTS issues ..."
+            break
+
+        if case('4bd32394'):
+            # TEMPLATE: "BcmCmUsRangingState:: T3NoRngRspEvent: ERROR - No initial ranging response from CMTS."
+            log_fault = True
+            log_description = "T3 time out because of no Initial RNG-RSP from CMTS."
+            log_suggestion = "Usually the US attnuation is too high or low, or some kind of CMTS issues ..."
+            break
+
+        # ---------------------------------------------------------------------
+        # MDD
+        # ---------------------------------------------------------------------
         if case('85b2bfec'):
             # TEMPLATE: "BcmCmDsChan:: MddKeepAliveFailTrans: hwRxId= <*> dcid= <*>"
             log_fault = True
@@ -154,6 +197,9 @@ def domain_knowledge(template_id, param_list):
             log_suggestion = "Usually downstream has some problems ..."
             break
 
+        # ---------------------------------------------------------------------
+        # DHCP, TFTP, TOD, IP, Networks
+        # ---------------------------------------------------------------------
         if case('48c6430a'):
             # TEMPLATE: "Logging event: DHCP FAILED - Request sent, No response; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
             log_fault = True
@@ -166,6 +212,33 @@ def domain_knowledge(template_id, param_list):
             log_fault = True
             log_description = "CM cannot be provisioned to get the IP address from DHCP server on CMTS side"
             log_suggestion = "Usually DHCP server down on CMTS side or upstream signal qulity is not good enough"
+            break
+
+        if case('50c4cbad'):
+            # TEMPLATE: "Tftp Client:: Send: ERROR - Request Retries > kTftpMaxRequestRetryCount ( <*> ) !"
+            log_fault = True
+            log_description = "Tftp client on CM cannot connect to the Tftp server behind the CMTS."
+            log_suggestion = "Check the Tftp server behind the CMTS."
+            break
+
+        # ---------------------------------------------------------------------
+        # QoS
+        # ---------------------------------------------------------------------
+        if case('3aad57b2'):
+            # TEMPLATE: "Bandwidth request failure! Status = <*>"
+            log_fault = True
+            log_description = "CM cannot request Upstream bandwidth from CMTS to send data."
+            log_suggestion = "Usually upstream signal qulity is not good enough while Ranging is good."
+            break
+
+        # ---------------------------------------------------------------------
+        # BPI+
+        # ---------------------------------------------------------------------
+        if case('c44cfdfa'):
+            # TEMPLATE: "Logging event: Auth Reject - Unauthorized SAID; CM-MAC= <*>; CMTS-MAC= <*>; CM-QOS= <*>; CM-VER= <*>; "
+            log_fault = True
+            log_description = "BPI+ Auth failed."
+            log_suggestion = "Check the BPI+ keys on CM, or disable it via CM Config file on CMTS."
             break
 
         if case():
