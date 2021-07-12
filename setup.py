37d75c3d7615ec5ff389b442987869b8e19c748f
Wei Han <wh935220@broadcom.com>
2021-07-12 10:49:12 +0800

    refactoring: phase_1.0: packageing framework

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""
+Description : Configuring, packaging and distributing project
+Author      : Wei Han <wei.han@broadcom.com>
+License     : MIT
+"""
+
+import setuptools
+
+setuptools.setup(use_scm_version=True)
