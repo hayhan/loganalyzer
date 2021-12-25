9d43c56d135f0e020857f7abc129f72483451a4a
wei Han <wh935220@broadcom.com>
2021-12-25 20:42:09 +0800

    Logwebserver: one script of predict for both loglab and oldschool

--- tests/pred.sh
+++ tests/pred.sh
@@ -11,5 +11,14 @@ esac
 source $SHELL_FOLDER/../../pyVirtEnvs/log_env/bin/activate
 export ANALYZER_DATA=$SHELL_FOLDER/../data
 
-#analyzer oldschool run
-analyzer loglab predict
+# echo $1 > $SHELL_FOLDER/../data/test/cm/tmp.txt
+
+# Use '=' instead of '==' to be back compatible with sh. It was also
+# verified with shell_exec() in php where '==' doesn't seem to  work.
+if [ $1 = machine ]
+then
+    analyzer loglab predict
+else
+    analyzer oldschool run
+fi
+
