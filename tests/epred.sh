e1db7357bcfdd227fb8381791093ee269022b4c9
Wei Han <wh935220@broadcom.com>
2022-05-09 22:45:23 +0800

    Loglab: Update epred script for eRouter

--- tests/epred.sh
+++ tests/epred.sh
@@ -21,15 +21,28 @@ latticecli -n "set Cm.ConsoleRedirect.AdminStatus 3" > /dev/null 2>&1
 
 # Wait for file transfer ready. Exit if timeout happens.
 counter=0
+rxing=0
 while true; do
     counter=$((counter+1))
     if [ "$counter" -gt 20 ]; then
+        echo "Timeout when receiving logs!"
         exit 1
     fi
     sleep 0.1
     m2=$(md5sum "$logfile")
-    if [ "$m1" != "$m2" ]; then
-        break
+    # Make sure the tftp rx starts, aka. md5 value begins to change
+    if [ "$rxing" -eq 0 ]; then
+        if [ "$m1" != "$m2" ]; then
+            rxing=1
+            m1="$m2"
+        fi
+    # Make sure the file rx ends, aka. md5 value doesn't change anymore
+    else
+        if [ "$m1" != "$m2" ]; then
+            m1="$m2"
+        else
+            break
+        fi
     fi
 done
 
